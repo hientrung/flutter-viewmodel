ce342bbd3025d9f5da702d324a20ab24699fe61c
hientrung <hientrung@hotmail.com>
2020-08-27 17:14:28 +0700

    add mask formatter

--- lib/src/textFormatter.dart
+++ lib/src/textFormatter.dart
@@ -9,6 +9,9 @@ abstract class TextFormatter<T> extends TextInputFormatter {
 
   ///Parser text to value
   dynamic parse(String text);
+
+  ///Check current text is valid to parse or not
+  bool isValid(String text) => true;
 }
 
 ///Process format number while typing
@@ -129,4 +132,168 @@ class NumberTextFormatter<T> extends TextFormatter<T> {
         return null;
     }
   }
+}
+
+///Handler format string by a masked text,
+///used to parse to other value type of TextFormatter
+class _StringFormat {
+  final String mask;
+  final Map<String, RegExp> keys;
+  final String holder;
+  final includeLiteral;
+  _StringFormat({
+    @required this.mask,
+    @required this.keys,
+    this.holder = '_',
+    this.includeLiteral = false,
+  })  : assert(holder != null),
+        assert(keys != null);
+
+  TextEditingValue formatEdit(TextEditingValue value) {
+    if (value.text == null || value.text.isEmpty) {
+      return value;
+    }
+
+    var text = value.text;
+    final arr = <RegExp>[];
+    for (var i = 0; i < mask.length; i++) {
+      if (keys.containsKey(mask[i])) {
+        arr.add(keys[mask[i]]);
+      }
+    }
+
+    //get raw text, position
+    var raw = '';
+    var ind = 0;
+    var pos = 0;
+    for (var i = 0; i < text.length && ind < arr.length; i++) {
+      if (text[i].contains(arr[ind])) {
+        ind++;
+        raw += text[i];
+        if (i < value.selection.end) {
+          pos++;
+        }
+      }
+    }
+
+    //format text and current position
+    text = format(raw);
+    var cur = 0;
+    ind = 0;
+    for (var i = 0; i < mask.length && ind < pos; i++) {
+      if (keys.containsKey(mask[i])) {
+        ind++;
+      }
+      cur++;
+    }
+    return TextEditingValue(
+      text: text,
+      selection: TextSelection.collapsed(offset: cur),
+    );
+  }
+
+  ///Check current text editing is valid or not
+  bool isValid(String text) {
+    if (text == null || text.isEmpty) {
+      return true;
+    }
+    if (text.length != mask.length) {
+      return false;
+    }
+    for (var i = 0; i < mask.length; i++) {
+      if (keys.containsKey(mask[i]) && !text[i].contains(keys[mask[i]])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  ///Format raw text to masked text
+  String format(String value) {
+    var s = '';
+    var c = 0;
+    for (var i = 0; i < mask.length; i++) {
+      if (keys.containsKey(mask[i])) {
+        if (c > -1 && c < value.length) {
+          if (value[c].contains(keys[mask[i]])) {
+            s += value[c];
+            c++;
+          } else {
+            s += holder;
+            c = -1; //invalid mask
+          }
+        } else {
+          s += holder;
+        }
+      } else {
+        s += mask[i];
+        if (includeLiteral) {
+          c++;
+        }
+      }
+    }
+    return s;
+  }
+
+  ///Parse masked text to raw text
+  String parse(String text) {
+    if (text == null || text.isEmpty) {
+      return '';
+    }
+    var s = '';
+    for (var i = 0; i < mask.length; i++) {
+      if (keys.containsKey(mask[i])) {
+        if (i < text.length && text[i].contains(keys[mask[i]])) {
+          s += text[i];
+        } else {
+          //invalid, maybe change mask
+          return '';
+        }
+      } else {
+        if (includeLiteral) {
+          s += mask[i];
+        }
+      }
+    }
+    return s;
+  }
+}
+
+///Process format masked text
+class MaskTextFormatter extends TextFormatter<String> {
+  final _StringFormat _fm;
+  MaskTextFormatter({
+    @required String mask,
+    Map<String, RegExp> keys,
+    String holder = '_',
+    bool includeLiteral = false,
+  })  : assert(mask != null),
+        _fm = _StringFormat(
+          mask: mask,
+          keys: keys ??
+              <String, RegExp>{
+                '*': RegExp(r'.'),
+                '0': RegExp(r'[0-9]'),
+                'A': RegExp(r'[a-zA-Z]'),
+                '#': RegExp(r'[0-9a-zA-Z]')
+              },
+          holder: holder,
+          includeLiteral: includeLiteral,
+        );
+
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.text == oldValue.text) return newValue;
+    return _fm.formatEdit(newValue);
+  }
+
+  @override
+  String format(String value) => _fm.format(value);
+
+  @override
+  dynamic parse(String text) => _fm.parse(text);
+
+  @override
+  bool isValid(String text) => _fm.isValid(text);
 }
